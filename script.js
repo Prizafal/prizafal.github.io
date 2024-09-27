a9e0f221cd8b7d056cca412fee5f842f527a249b
Prizafal <root@prizafal.com>
2024-09-27 02:13:00 -0400

    update(index.html) - carousel text
    Added scripting to change the tagline under my username whenever the site reloads, with funny comments

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    const texts = [
+        "idk what to put here ngl",
+        "WTF is a dumbass",
+        "🍞",
+        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        "sudo rm -rf */",
+        "read the fucking docs (RTFD)",
+        "looks like a layer 8 error chief",
+        "i want soup",
+        "how are you? actually nvm idrc",
+        "ERROR 418",
+        "always carry a towel",
+        "42",
+        "todo: add funny comment",
+        "im going back to bed"
+
+    ];
+
+    const randomIndex = Math.floor(Math.random() * texts.length);
+    document.getElementById("random-text").innerText = texts[randomIndex];
+};

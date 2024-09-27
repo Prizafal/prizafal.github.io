b2bd53335f136e5a2b901d1574b72e3818a667c5
Prizafal <root@prizafal.com>
2024-09-27 03:15:48 -0400

    update(script.js) - more funiies

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ window.onload = function() {
         "always carry a towel",
         "42",
         "todo: add funny comment",
-        "im going back to bed"
+        "im going back to bed",
+        "dev focus",
+        "You don't have permission to use this.",
+        "black and orange youtube",
+        "i might be stupid",
+        "so... hows the weather",
+        'Click <a href="index.html">here</a> for a cookie',
+        'Click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">here</a> for a cookie',
+        "This is not the webpage you are looking for",
+        "Have you ever heard the tradgedy of Darth Plageius the Wise?",
+        "[insert funny comment here]"
 
     ];
 
